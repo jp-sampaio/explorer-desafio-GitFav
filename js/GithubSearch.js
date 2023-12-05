fd9cad0a198efd94f0e8720ba30fa7bc89058d02
JP Sampaio <108679561+jp-sampaio@users.noreply.github.com>
2023-12-05 06:53:51 -0300

    Primeira parte do projeto concluido

---
+++ js/GithubSearch.js
@@ -0,0 +1,14 @@
+export class GithubSearch {
+  static search(userName) {
+    const endPoint = `https://api.github.com/users/${userName}`;
+    
+    fetch(endPoint)
+      .then( data => data.json())
+      .then( ({login, name, public_repos, followers}) => ({
+        login,
+        name,
+        public_repos,
+        followers
+      }));
+  }
+}
